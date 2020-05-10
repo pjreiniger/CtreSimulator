0667605aacac27536d40e5b076df12708c375ea5
PJ Reiniger <pj.reiniger@gmail.com>
2020-05-10 15:33:14 -0400

    Fixing explicit warning

--- src/main/native/include/CtreSimMocks/CtreCANCoderWrapper.h
+++ src/main/native/include/CtreSimMocks/CtreCANCoderWrapper.h
@@ -13,7 +13,7 @@ class CtreCANCoderWrapper : public BaseCtreWrapper
 public:
     using BaseCtreWrapper::Send;
 
-    CtreCANCoderWrapper(int aDeviceId);
+    explicit CtreCANCoderWrapper(int aDeviceId);
 
     void Receive(const std::string& aName, uint8_t* aBuffer, int aSize);
     void Send(const std::string& aName, uint8_t* aBuffer, int aSize);
